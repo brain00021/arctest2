44b3f17d83b0d4396dc6ed1f71fbc26886e8ae7d
Brian Li <brain00021@gmail.com>
2023-03-22 08:12:23 +0000

    update setting

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 $(function () {
+  let currentEventData = [];
+  let month = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
   let getAuth = async () => {
     let token = `264c77f740cc1f02cac8f0a7e30ccdcd2f20dcf5`;
     let auth = "";
@@ -56,8 +71,8 @@ $(function () {
   const getAll = async (searchMonth) => {
     let key = await getAuth();
     let data = await getData(key, searchMonth);
-
-    getRenderCard(data);
+    currentEventData = data;
+    getRenderCard(data, searchMonth);
   };
 
   const getRwdImg = (img) => {
@@ -76,36 +91,86 @@ $(function () {
       return undefined;
     }
   };
-  const getRenderCard = async (data) => {
-    let cardWrapper = $("#card-wrapper");
-    console.log("資料", data);
-    cardWrapper.empty();
 
-    if (data.length > 0) {
-      for (let i = 0; i < data.length; i++) {
-        // console.log(description, "description");
-        var encodedStr = data[i].description;
-        var decoded = $("<div/>").html(encodedStr).text();
-        var rwdImg = getRwdImg(data[i].images);
-        cardWrapper.append(`<div class="card"><div class='title'>${
-          data[i].title
-        }</div>
-        <div class="img">${
-          rwdImg ? `<img src="${rwdImg}"/>` : `<div class="errorImg"></div>`
-        } </div>
-        <div class="time">${data[i].date.slice(0, 10)}</div>
-        <div class='description'>${decoded}</div></div>`);
-      }
+  const getRenderDialog = function () {
+    const currentDialogItem = $(this).data("set");
+
+    let currentEvent = currentEventData.find(
+      (item) => new Date(item.date).getDate() === currentDialogItem
+    );
+    $(".dialog-body").empty();
+    if (currentEvent) {
+      $("html,body").css("overflow", "hidden");
+      $(".dialog-bg").fadeIn();
+      var encodedStr = currentEvent.description;
+      var decoded = $("<div/>").html(encodedStr).text();
+      currentEventHTML = ` <div class="card">
+
+      <img src="${getRwdImg(currentEvent.images)}" class="card-img-top" alt="${
+        currentEvent.title
+      }">
+      <div class="card-body">
+        <h5 class="card-title">${currentEvent.title}</h5>
+        <p class="date">${currentEvent.date.slice(0, 10)}</p>
+        <p class="card-text">${decoded}</p>
+      </div>
+    </div>`;
+      $(".dialog-body").append(currentEventHTML);
     } else {
-      cardWrapper.append(
-        `<div class="notice"> There is no event this month</div>`
-      );
+      return false;
     }
+
+    console.log("getRenderDialog", currentEvent);
   };
 
-  const searchFliter = () => {
+  const getRenderCard = async (data, searchMonth) => {
+    console.log(searchMonth, data, "searchMonth");
+    const getCurrentMonthDays = getCurrentMonth(searchMonth);
+    $(".calendar").empty();
+    for (let i = 0; i < getCurrentMonthDays; i++) {
+      let currentEvent = data.find(
+        (item) => new Date(item.date).getDate() === i + 1
+      );
+      let currentEventHTML = `<div></div>`;
+      if (currentEvent) {
+        var encodedStr = currentEvent.description;
+        var decoded = $("<div/>").html(encodedStr).text();
+        currentEventHTML = ` <div class="card dialogOpen" data-set="${i + 1}">
+        
+        <img src="${getRwdImg(
+          currentEvent.images
+        )}" class="card-img-top" alt="${currentEvent.title}">
+        <div class="card-body">
+          <h5 class="card-title">${currentEvent.title}</h5>
+        </div>
+      </div>`;
+      }
+      $(".calendar").append(`<div class='calendar-item'>
+          <div class="calendar-title">${i + 1}</div>
+          ${currentEventHTML}
+        </div>`);
+    }
+
+    loadingStatus(false);
+    $(".calendar").slideDown();
+  };
+  const loadingStatus = function (status) {
+    if (status) {
+      $(".loading").show();
+    } else {
+      $(".loading").hide();
+    }
+  };
+  const searchFliter = function () {
     let text = document.getElementById("mothText");
-    let searchMonth = text.value;
+    let currentMonthNumber = $(this).data("set") || 1;
+    $(".mouthOption .searchButton")
+      .removeClass("active")
+      .eq(currentMonthNumber - 1)
+      .addClass("active");
+    let searchMonth = currentMonthNumber;
+    loadingStatus(true);
+    $(".calendar").slideUp();
     if (
       isNaN(searchMonth) ||
       Number(searchMonth) > 12 ||
@@ -113,24 +178,44 @@ $(function () {
     ) {
       alert("Please Input Month's Number");
     } else {
-      let month = [
-        "JAN",
-        "FEB",
-        "MAR",
-        "APR",
-        "MAY",
-        "JUN",
-        "JUL",
-        "AUG",
-        "SEP",
-        "OCT",
-        "NOV",
-        "DEC",
-      ];
-      $(".bigTitle").html(`<h1>${month[searchMonth - 1]} EVENT LIST</h1>`);
+      $(".bigTitle").html(
+        `<h1>2023 ARENA RACING ${month[searchMonth - 1]} EVENT</h1>`
+      );
+
       getAll(searchMonth);
     }
   };
+  const getRenderMonthButton = function () {
+    for (let i = 0; i < month.length; i++) {
+      $(".mouthOption").append(
+        `<div class="searchButton" data-set="${i + 1}">${month[i]}</div>`
+      );
+    }
+  };
+
+  const getCurrentMonth = (searchMonth) => {
+    function getDaysInMonth(year, month) {
+      return new Date(year, month, 0).getDate();
+    }
+
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = searchMonth; // 👈️ months are 0-based
+
+    // 👇️ Current Month
+    const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth);
+    console.log(daysInCurrentMonth); // 👉️ 31
+    return daysInCurrentMonth;
+  };
+  getRenderMonthButton();
+  // addEventListner
+  $(".calendar").on("click", ".dialogOpen", getRenderDialog);
+  $(".mouthOption").on("click", ".searchButton", searchFliter);
+  $(".close").on("click", function () {
+    $(".dialog-bg").fadeOut();
+    $("html,body").css("overflow", "auto");
+  });
 
   $("#search").on("click", searchFliter);
+  searchFliter();
 });
